921b6343b2663a4e63a63540ef0e1984aba518a0
Nick Poorman <nickpoorman@gmail.com>
2013-04-06 16:36:28 -0400

    updated

--- modules/battlePoints.js
+++ modules/battlePoints.js
@@ -1,33 +1,22 @@
+// user1 should be you, user2 is the other guy
+// return 0 if you win, 1 if the other guy won
+module.exports = function(user1, user2) {
 
-module.exports = function(user1,user2) {
-
-
-while (user1 > 0 && user2 > 0) 
-		{
-			user1_points = Math.floor((Math.random()*10)+1);
-			user2_points = Math.floor((Math.random()*10)+1);
-			if (user1_points > user2_points)
-				{
-					user2 = user2 - 1;
-				}
-			if (user2_points > user1_points)
-				{
-					user1 = user1 - 1;	
-				}
-
-
+	while (user1 > 0 && user2 > 0) {
+		user1_points = Math.floor((Math.random() * 10) + 1);
+		user2_points = Math.floor((Math.random() * 10) + 1);
+		if (user1_points > user2_points) {
+			user2 = user2 - 1;
 		}
-		if (user1 > 0)
-		{
-			return 0;
+		if (user2_points > user1_points) {
+			user1 = user1 - 1;
 		}
-		if (user2 > 0)
-		{
-			return 1;
-		}
-
-
-
+	}
+	if (user1 > 0) {
+		return 0;
+	}
+	if (user2 > 0) {
+		return 1;
+	}
 
 };
-
